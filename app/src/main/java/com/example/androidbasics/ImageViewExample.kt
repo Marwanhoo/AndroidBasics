233d99c929905cb503a13b720dd38d4fbc8f47b9
Marwanhoo <Muh.Marwanhoo@gmail.com>
2025-06-05 16:32:46 +0300

    image view example

---
+++ app/src/main/java/com/example/androidbasics/ImageViewExample.kt
@@ -0,0 +1,29 @@
+package com.example.androidbasics
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.ImageView
+
+class ImageViewExample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_image_view_example)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnImage = findViewById<Button>(R.id.btnImage)
+        val ivImageExample = findViewById<ImageView>(R.id.ivImageExample)
+
+        btnImage.setOnClickListener {
+            ivImageExample.setImageResource(R.drawable.image_example)
+        }
+    }
+}
